a997f39a0bc093bcea4aa869baf2bf2bbda1fef0
galaxyme-github <littleyoungman2019@outlook.com>
2021-06-06 02:06:59 -0700

    fix

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -49,7 +49,8 @@ class MailResetPasswordNotification extends ResetPassword
             ->line("Hola! Recibió este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para su cuenta." )
             ->action('Restablecer la contraseña', $link )
             ->line("Este enlace de restablecimiento de contraseña caducará en ".config('auth.passwords.users.expire')." minutos" )
-            ->line("Si no solicitó un restablecimiento de contraseña, no es necesario realizar ninguna otra acción." );
+            ->line("Si no solicitó un restablecimiento de contraseña, no es necesario realizar ninguna otra acción." )
+            ->salutation('kkk');
     }
 
     /**
